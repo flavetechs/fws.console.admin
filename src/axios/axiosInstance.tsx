cc121fb5c624c7ee1cf649a325c290df7901194c
flave1 <favouremmanuel433@gmail.com>
2023-03-20 17:33:29 +0100

    ..

--- src/axios/axiosInstance.tsx
+++ src/axios/axiosInstance.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 const axiosInstance = axios.create({
-    baseURL: 'http://flavetechs.com/fws/client',
-    // baseURL:'https://localhost:44357/',
+    // baseURL: 'http://flavetechs.com/fws/client',
+    baseURL:'https://localhost:44357/',
     headers: {
         Authorization: '',
     },
--- src/components/landing-page/home.tsx
+++ src/components/landing-page/home.tsx
@@ -43,7 +43,7 @@ const Home = () => {
       <section id="hero" className="d-flex align-items-center">
         <div className="container" data-aos="zoom-out" data-aos-delay="100">
           <h1>Welcome to <span>FlaveConsole</span></h1>
-          <h2>We are team of talented designers making websites with Bootstrap</h2>
+          <h2>Looking for a technology partner to help you achieve your goals ?</h2>
           <div className="d-flex">
             <a href="#about" className="btn-get-started scrollto">Get Started</a>
             <a href="https://www.youtube.com/watch?v=jDDaplaOz7Q" className="glightbox btn-watch-video"><i className="bi bi-play-circle"></i><span>Watch Video</span></a>
